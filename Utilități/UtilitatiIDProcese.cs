650474e0a40de5011e2dfe003a047ba6f6cf9c74
Tudor <114757688+TATGM@users.noreply.github.com>
2025-05-04 20:12:55 +0300

    Update UtilitatiIDProcese.cs

--- Utilități/UtilitatiIDProcese.cs
+++ Utilități/UtilitatiIDProcese.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ProiectFinal.Utilități
 {
     static class UtilitatiIDProcese
     {
-        public static ProtoComm.ProcessId FindMaxRank(IEnumerable<ProtoComm.ProcessId> procese)
+        public static ProtoComm.ProcessId GasireRangMaxim(IEnumerable<ProtoComm.ProcessId> procese)
         {
             return (procese.Count() == 0) ? null : procese.OrderBy(procId => procId.Rank).LastOrDefault();
         }
