5abf0b10b53f6609ada01e16a54409641813589d
Tudor <114757688+TATGM@users.noreply.github.com>
2025-05-25 10:13:42 +0300

    Add files via upload

---
+++ Utilități/UtilitatiStareEpocaConsensus.cs
@@ -0,0 +1,14 @@
+﻿using ProiectFinal.Abstracții;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProiectFinal.Utilități
+{
+    static class UtilitatiStareEpocaConsensus
+    {
+        public static StareEpocaConsensus GasireCelMaiMare(IEnumerable<StareEpocaConsensus> stari)
+        {
+            return (stari.Count() == 0) ? null : stari.OrderBy(stare => stare.ValoareTimestamp).Last();
+        }
+    }
+}
